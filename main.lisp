616b15ff74983997d52798b70913f76bcaf5ec47
Vageesh D C <dc.genius@gmail.com>
2012-10-27 15:19:38 +0530

    working substitution

--- main.lisp
+++ main.lisp
@@ -193,6 +193,7 @@
 	))
     
 (defun searchAndReplace ( func deri val)
+	;(setf state 0)
 	(
 		if(null func) func
 		(
@@ -208,19 +209,30 @@
 				)
 			(
 				if(listp func)
+				(	
+					if(listp (searchAndReplace (car func) deri val))
+					(	
+						if(eq (length (searchAndReplace (car func) deri val)) 1)
+							(
+								cons (car (searchAndReplace (car func) deri val)) (searchAndReplace (cdr func) deri val)
+							)
+						(cons (searchAndReplace (car func) deri val) (searchAndReplace (cdr func) deri val))
+					)
 					(cons (searchAndReplace (car func) deri val) (searchAndReplace (cdr func) deri val))
+				)
 				func
 			)
 		)
 	))
 
 (defun substitution	(func deri)
+	(setf intd (starFun deri 0))
 	(
-		if(null (searchpattern func deri)) nil
+		if(null (searchpattern func intd)) nil
 		(
-			if(null (xpresent (searchAndReplace func deri 'z)))
+			if(null (xpresent (searchAndReplace func intd 'z)))
 				(
-					searchAndReplace (starFun (searchAndReplace (searchAndReplace func deri 'z) 'z 'x) 0) 'x deri
+					searchAndReplace (starFun (searchAndReplace (searchAndReplace func intd 'z) 'z 'x) 0) 'x intd
 				)
 			nil	
 		)
@@ -247,22 +259,29 @@
 								    if(and (null (xpresent (third str)) ) (null (xpresent (second str))) ) (list '* str 'x)
 								    (
 										; adding the rules for substitution here
-										
-										
-									    ;UV rule!!
-									    ;modify global parameter here
-									    if(eq (isNilPresent (starFun (list '* (starFun (car (tillN1 (cdr str))) 0) (diff  (car(last str)))) (+ 1 coun) ) ) T)
-										    ( ;print "2"
-											    if(eq (isNilPresent (starFun (list '* (starFun (car (last str)) 0) (diff (car (tillN1 (cdr str))) )) (+ 1 coun) ) ) T) nil
-											    (
-												    list '+ (list '* (starFun (car (last str)) 0) (car (tillN1  (cdr str) )) )  (list '* (list (* -1 (^ -1 (mod coun 2)))) ( starFun (list '* (starFun (car (last str )) 0)  (diff (car (tillN1 (cdr str))))) 0 ))
-											    )
+										if(null (substitution (third str) (second str)))
+											(
+												if(null (substitution (second str) (third str)))
+												(
 											
-										    )
-									    ( ;print "1"
-									        ;list '* (starFun (tillN1  str) 0) (car (last str) ) 
-										    list '+  (list '* (starFun (car (tillN1  (cdr str) ) ) 0) (car (last str) ) ) (list '* (list (* -1 (^ -1 (mod coun 2)))) (starFun (list '* (starFun (car (tillN1  (cdr str))) 0) (diff (car (last str))) ) 0))
-									    )
+													;UV rule!!
+													;modify global parameter here
+													if(eq (isNilPresent (starFun (list '* (starFun (car (tillN1 (cdr str))) 0) (diff  (car(last str)))) (+ 1 coun) ) ) T)
+														( ;print "2"
+															if(eq (isNilPresent (starFun (list '* (starFun (car (last str)) 0) (diff (car (tillN1 (cdr str))) )) (+ 1 coun) ) ) T) nil
+															(
+																list '+ (list '* (starFun (car (last str)) 0) (car (tillN1  (cdr str) )) )  (list '* (list (* -1 (^ -1 (mod coun 2)))) ( starFun (list '* (starFun (car (last str )) 0)  (diff (car (tillN1 (cdr str))))) 0 ))
+															)
+														
+														)
+													( ;print "1"
+														;list '* (starFun (tillN1  str) 0) (car (last str) ) 
+														list '+  (list '* (starFun (car (tillN1  (cdr str) ) ) 0) (car (last str) ) ) (list '* (list (* -1 (^ -1 (mod coun 2)))) (starFun (list '* (starFun (car (tillN1  (cdr str))) 0) (diff (car (last str))) ) 0))
+													)
+												)
+												(substitution (second str) (third str))
+											)
+											(substitution (third str) (second str))
 								    )
 							    )
 						    )
